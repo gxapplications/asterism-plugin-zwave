48207e2fe0c69486574e16953e529fa7683e06bd
Xavier <xavier.gouley@gmail.com>
2022-04-14 23:42:06 +0200

    siren, and new action

---
+++ lib/scenarii/siren-6-tones-action/edit-form.jsx
@@ -0,0 +1,130 @@
+'use strict'
+
+import PropTypes from 'prop-types'
+import React from 'react'
+import { Select, Preloader, Row } from 'react-materialize'
+import uuid from 'uuid'
+
+class ZwaveSiren6TonesActionEditForm extends React.Component {
+  constructor (props) {
+    super(props)
+    this.zwaveService = props.services()['asterism-plugin-zwave']
+
+    this.state = {
+      compatibleNodes: [],
+      ready: false,
+      nodeIds: [],
+      tone: this.props.instance.data.tone || 255,
+      wait: this.props.instance.data.wait
+    }
+
+    this._mounted = false
+  }
+
+  componentDidMount () {
+    this._mounted = true
+
+    this.zwaveService.getNodesByProvidedFunctions(['playTone'])
+      .then((nodes) => {
+        if (this._mounted) {
+          this.setState({
+            compatibleNodes: nodes.length ? nodes : [{ nodeid: 0, name: 'No compatible device available' }],
+            ready: true
+          })
+          if (nodes.length === 1) {
+            this.props.instance.data.nodeIds[0] = nodes[0].nodeid
+            this.nameChange()
+            this.setState({
+              nodeIds: this.props.instance.data.nodeIds
+            })
+          }
+        }
+      })
+      .catch(console.error)
+  }
+
+  componentWillUnmount () {
+    this._mounted = false
+  }
+
+  render () {
+    const { compatibleNodes, ready } = this.state
+    const { instance } = this.props
+
+    return ready ? (
+      <Row className='section card form'>
+        <br className='col s12 m12 l12' key={uuid.v4()} />
+        {compatibleNodes.length > 0 ? instance.data.nodeIds.map((nodeId, idx) => (
+          <Select key={uuid.v4()} s={12} m={6} l={4} label={`Z-wave device #${idx + 1}`} icon='battery_4_bar'
+            onChange={this.nodeChanged.bind(this, idx)} value={`${nodeId}`}>
+            {compatibleNodes.map((node, i) => (
+              <option key={uuid.v4()} value={node.nodeid}>{node.name}</option>
+            ))}
+            <option key={uuid.v4()} value='0'>(Remove it)</option>
+          </Select>
+        )) : (
+          <div>Compatible devices not found on the network.</div>
+        )}
+        <Select key={uuid.v4()} s={12} m={6} l={4}
+          label={`Z-wave device #${instance.data.nodeIds.length + 1}`} icon='battery_4_bar'
+          onChange={this.nodeChanged.bind(this, instance.data.nodeIds.length)} value={''}>
+          <option key={uuid.v4()} value='' disabled>(Choose one to add)</option>
+          {compatibleNodes.map((node, idx) => (
+            <option key={uuid.v4()} value={node.nodeid}>{node.name}</option>
+          ))}
+        </Select>
+
+        TODO !0: tone and wait params
+      </Row>
+    ) : (
+      <div className='valign-wrapper centered-loader'>
+        <Preloader size='big' />
+      </div>
+    )
+  }
+
+  nodeChanged (index, event) {
+    const newNodeId = parseInt(event.currentTarget.value)
+    if (newNodeId > 0) {
+      this.props.instance.data.nodeIds[index] = newNodeId
+    } else {
+      const nodeIds = this.props.instance.data.nodeIds.filter((nodeId, idx) => idx !== index)
+      if (nodeIds.length > 0) { // avoid to remove all nodes (1 min needed)
+        this.props.instance.data.nodeIds = nodeIds
+      }
+    }
+    this.setState({
+      nodeIds: this.props.instance.data.nodeIds
+    })
+    this.nameChange()
+  }
+
+  nameChange () {
+    if (this.props.instance.data.nodeIds.length === 0) {
+      this.props.instance.data.name = 'Misconfigured siren 6 tones action'
+      this.props.highlightCloseButton()
+      return
+    }
+    const nodeNames = this.state.compatibleNodes
+      .filter((node) => this.props.instance.data.nodeIds.includes(node.nodeid))
+      .map((node) => `"${node.name}"`)
+
+    this.props.instance.data.name = `Siren for [${nodeNames.join(',')}] with tone "${this.props.instance.data.tone}"`
+
+    this.props.highlightCloseButton()
+  }
+}
+
+ZwaveSiren6TonesActionEditForm.propTypes = {
+  instance: PropTypes.object.isRequired,
+  services: PropTypes.func.isRequired,
+  highlightCloseButton: PropTypes.func
+}
+
+ZwaveSiren6TonesActionEditForm.defaultProps = {
+  highlightCloseButton: () => {}
+}
+
+ZwaveSiren6TonesActionEditForm.label = 'Z-wave Philio PSE04 siren control'
+
+export default ZwaveSiren6TonesActionEditForm
--- lib/setting-panels/philio-pse04.jsx
+++ lib/setting-panels/philio-pse04.jsx
@@ -5,14 +5,16 @@ import cx from 'classnames'
 import BaseSettingPanel from './base'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { Checkbox, Row, Select } from 'react-materialize'
+import { Checkbox, Row, Select, Button } from 'react-materialize'
 
 import NameLocation from './name-location'
+import alarmMapper from '../products/philio-pse04-alarm-mapper'
 
 class PhilioPse04SettingPanel extends BaseSettingPanel {
   constructor (props) {
     super(props, PhilioPse04SettingPanel.configurations)
     this.withBatteryLevelSupport()
+    this.withAlarmSupport(alarmMapper)
 
     this.state = {
       ...this.state,
@@ -48,7 +50,7 @@ class PhilioPse04SettingPanel extends BaseSettingPanel {
         range: {
           'min': [0, 1],
           '35%': [15, 5],
-          '65%': [60, 15],
+          '62%': [60, 15],
           'max': [250]
         },
         pips: { // Show a scale with the slider
@@ -69,7 +71,7 @@ class PhilioPse04SettingPanel extends BaseSettingPanel {
         range: {
           'min': [0, 1],
           '35%': [15, 5],
-          '65%': [60, 15],
+          '62%': [60, 15],
           'max': [250]
         },
         pips: { // Show a scale with the slider
@@ -90,7 +92,7 @@ class PhilioPse04SettingPanel extends BaseSettingPanel {
         range: {
           'min': [0, 1],
           '35%': [15, 5],
-          '65%': [60, 15],
+          '62%': [60, 15],
           'max': [250]
         },
         pips: { // Show a scale with the slider
@@ -111,7 +113,7 @@ class PhilioPse04SettingPanel extends BaseSettingPanel {
         range: {
           'min': [0, 1],
           '35%': [15, 5],
-          '65%': [60, 15],
+          '62%': [60, 15],
           'max': [250]
         },
         pips: { // Show a scale with the slider
@@ -124,46 +126,115 @@ class PhilioPse04SettingPanel extends BaseSettingPanel {
       },
       this.changeTemperatureDifferentialReportConfiguration.bind(this)
     )
+    this.plugConfigurationSlider(
+      'sound-duration-slider',
+      PhilioPse04SettingPanel.configurations.SOUND_DURATION,
+      6,
+      {
+        range: {
+          'min': [0, 1],
+          '35%': [15, 5],
+          '62%': [60, 15],
+          'max': [250]
+        },
+        pips: { // Show a scale with the slider
+          mode: 'steps',
+          stepped: true,
+          density: 2,
+          format: wNumb({ decimals: 1, edit: (v) => v < 0.5 ? 'Off' : `${v*30}`.split('.')[0] + '?' })
+        },
+        tooltips: wNumb({ decimals: 1, edit: (v) => v < 0.5 ? 'Off' : `${v*30}`.split('.')[0] + '?' })
+      },
+      this.changeSoundDuration.bind(this)
+    )
   }
 
   render () {
     const { nodeId, animationLevel, theme, productObjectProxy } = this.props
-    const { batteryPercent, batteryIcon, panelReady, playSoundControlConf, customerFunctionConf, temperature } = this.state
-    const { soundLevel, soundTone } = playSoundControlConf || {}
-    const { disableTriggerAlarm, disableSound, temperatureUnitInCelsius } = customerFunctionConf || {}
+    const { batteryPercent, batteryIcon, panelReady, temperature } = this.state
+    const { soundLevel, soundTone } = this.state.playSoundControlConf || {}
+    const { disableTriggerAlarm, disableSound, temperatureUnitInCelsius } = this.state.customerFunctionConf || {}
+    const { alarmStatuses } = this.state.alarms
+
+    let alarming = (alarmStatuses['14'] === true) ? 'Siren alerts' : 'Clear'
+    alarming = (alarmStatuses['7'] === true) ? 'Burglar alarm' : ((alarmStatuses['14'] === 'Unknown') ? 'Unknown' : alarming)
 
     return panelReady ? (
       <div>
         <Row>
           <h4 className='col s12 m12 l8'>PSE04 Siren</h4>
+          <div className='col s12 m9 l5'>Siren #{nodeId} state actually "{alarming}".</div>
           <div className='right'>
             <i className={cx('material-icons', batteryIcon)}>{batteryIcon}</i>&nbsp;{batteryPercent}% &nbsp; / {temperature}
           </div>
           <NameLocation theme={theme} animationLevel={animationLevel} productObjectProxy={productObjectProxy} />
         </Row>
 
+        <Row>
+          <Button s={12} m={6} l={6} onClick={() => this.props.productObjectProxy.playTone(0)}>Stop sound</Button>
+          <Button s={12} m={6} l={6} onClick={() => this.props.productObjectProxy.playTone(255)}>Test siren</Button>
+          <Button s={12} m={6} l={6} onClick={() => this.props.productObjectProxy.playTone(5)}>Test "Dingdong"</Button>
+          <Button s={12} m={6} l={6} onClick={() => this.props.productObjectProxy.playTone(6)}>Test "Beep"</Button>
+        </Row>
+
         <h5>Siren / sound</h5>
         <Row className='section card form'>
-          TODO !0: PLAY_SOUND_CONTROL => sound level (dropdown 1-2-3) {soundLevel}
-
-          TODO !0: PLAY_SOUND_CONTROL => sound tone (dropdown 7 choices stop-fire-ambulance-police-alarm-dingdong-beep) {soundTone}
-
-          TODO !0: SOUND_DURATION (slider)
-
-          <div className='col s12 m6 l3'>
+          <div className='col s12 m5 l5'>
             <Checkbox
               className='filled-in' value='1' label='Disable trigger alarm'
               onChange={() => { this.changeCustomerFunction({ disableTriggerAlarm: !disableTriggerAlarm }) }}
               checked={disableTriggerAlarm}
             />
           </div>
-          <div className='col s12 m6 l3'>
+
+          <div className='col s12 m5 l5'>
             <Checkbox
               className='filled-in' value='2' label='Disable sound'
               onChange={() => { this.changeCustomerFunction({ disableSound: !disableSound }) }}
               checked={disableSound}
             />
           </div>
+
+          <Select
+            s={12} m={6} label='Default siren level'
+            onChange={(v) => this.changePlaySoundControl({ soundLevel: parseInt(v.currentTarget.value) })}
+            value={`${soundLevel}`}
+          >
+            <option disabled>Default siren level</option>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+          </Select>
+
+          <Select
+            s={12} m={6} label='Default siren tone'
+            onChange={(v) => this.changePlaySoundControl({ soundTone: parseInt(v.currentTarget.value) })}
+            value={`${soundTone}`}
+          >
+            <option disabled>Default siren tone</option>
+            <option value={0}>Stop</option>
+            <option value={1}>Fire</option>
+            <option value={2}>Ambulance</option>
+            <option value={3}>Police</option>
+            <option value={4}>Alarm</option>
+            <option value={5}>Ding dong</option>
+            <option value={6}>Beep</option>
+          </Select>
+
+          <div className='col s12'>
+            Sound duration
+          </div>
+          <div className='col s12 slider'>
+            <div id={`sound-duration-slider-${nodeId}`} />
+          </div>
+
+          TODO !0 : tone set, comment ca fait doublon (ou pas) avec "default siren tone" ?
+          TODO !0 : volume set, comment ca fait doublon (ou pas) avec "default siren level" 1-3 ?
+          <br />
+          <Button onClick={() => this.props.productObjectProxy.setToneAndVolume(1, 10)}>set 1, fire, v10</Button>
+          <Button onClick={() => this.props.productObjectProxy.setToneAndVolume(4, 5)}>set 4, alarm, v5</Button>
+          <Button onClick={() => this.props.productObjectProxy.setToneAndVolume(5, 10)}>set 5, dingdong, v10</Button>
+          <Button onClick={() => this.props.productObjectProxy.setToneAndVolume(6, 10)}>set 6, beep, v10</Button>
         </Row>
 
         <h5>Status reporting</h5>
@@ -233,9 +304,22 @@ class PhilioPse04SettingPanel extends BaseSettingPanel {
   changeCustomerFunction (customerFunctionConf) {
     this.props.productObjectProxy.setCustomerFunctionConf(customerFunctionConf)
       .then(() => {
-        this.setState({ customerFunctionConf })
+        this.setState({ customerFunctionConf: { ...this.state.customerFunctionConf, ...customerFunctionConf } })
       })
       .catch(console.error)
+  }
+
+  changePlaySoundControl (playSoundControlConf) {
+    this.props.productObjectProxy.setPlaySoundControlConf(playSoundControlConf)
+      .then(() => {
+        this.setState({ playSoundControlConf: { ...this.state.playSoundControlConf, ...playSoundControlConf } })
+      })
+      .catch(console.error)
+  }
+
+  changeSoundDuration (value) {
+    const index = PhilioPse04SettingPanel.configurations.SOUND_DURATION
+    return this.changeConfiguration(index, value[0], parseInt) // TODO !0: /30 ?
   }
 }
 
